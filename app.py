9bd37b0e4dc60cef9e9dbe079be56f74a1eeca52
Gustavo Ribeiro <jornalistagustavoribeiro@gmail.com>
2020-10-07 10:13:59 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -5,16 +5,50 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import requests
-import sqlalchemy
 import plotly.express as px
 import plotly.graph_objects as go
 
-# reading the dataset 
+# reading the dataset with the specified header by brasil.io
 
-engine = sqlalchemy.create_engine("URI")
+url = "https://brasil.io/api/dataset/covid19/caso_full/data/?city=Ribeir%C3%A3o+Preto&format=json"
+headers = {"User-Agent": "python-urllib"}
+r = requests.get(url, headers=headers)
 
-caso_full = pd.read_sql_query('select * from caso_full', con=engine)
-tabela = pd.read_sql_query('select * from tabela', con=engine)
+# get only the column "results" from the json into a dataframe
+
+caso_full = pd.DataFrame(r.json()["results"])
+
+# cleaning dataframe
+
+caso_full = caso_full.drop(
+    ['city', 'city_ibge_code', 
+     'epidemiological_week', 
+     'estimated_population_2019', 
+     'is_last', 'is_repeated', 
+     'last_available_confirmed_per_100k_inhabitants', 
+     'last_available_date', 
+     'last_available_death_rate', 
+     'order_for_place', 
+     'place_type', 
+     'state'], axis=1)
+
+# adding moving average
+
+caso_full['mediaMovelCasos'] = caso_full.rolling(window=7)['new_confirmed'].mean()
+caso_full['mediaMovelMortes'] = caso_full.rolling(window=7)['new_deaths'].mean()
+
+# creating the table dataframe
+
+tabela = caso_full.drop(
+    ['new_confirmed',
+     'new_deaths',
+     'mediaMovelCasos',
+     'mediaMovelMortes'], axis=1)
+
+tabela = tabela.rename(columns=
+                       {'date':'Data', 
+                        'last_available_confirmed':'Casos', 
+                        'last_available_deaths':'Mortes'})
 
 # creating charts
 
