e25031db53618f05554b34288be9f423d4e4eaa8
Gustavo Ribeiro <jornalistagustavoribeiro@gmail.com>
2020-10-15 23:05:35 -0300

    atualizacao de fonte

--- app.py
+++ app.py
@@ -21,17 +21,17 @@ caso_full = pd.DataFrame(r.json()["results"])
 # cleaning dataframe
 
 caso_full = caso_full.drop(
-    ['city', 'city_ibge_code',
-     'epidemiological_week',
-     'estimated_population',
-     'estimated_population_2019',
-     'is_last', 'is_repeated',
-     'last_available_confirmed_per_100k_inhabitants',
-     'last_available_date',
-     'last_available_death_rate',
-     'order_for_place',
-     'place_type',
-     'state'], axis=1)
+	['city', 'city_ibge_code',
+	 'epidemiological_week',
+	 'estimated_population',
+	 'estimated_population_2019',
+	 'is_last', 'is_repeated',
+	 'last_available_confirmed_per_100k_inhabitants',
+	 'last_available_date',
+	 'last_available_death_rate',
+	 'order_for_place',
+	 'place_type',
+	 'state'], axis=1)
 
 # adding moving average
 
@@ -41,50 +41,50 @@ caso_full['mediaMovelMortes'] = caso_full.rolling(window=7)['new_deaths'].mean()
 # creating the table dataframe
 
 tabela = caso_full.drop(
-    ['new_confirmed',
-     'new_deaths',
-     'mediaMovelCasos',
-     'mediaMovelMortes'], axis=1)
+	['new_confirmed',
+	 'new_deaths',
+	 'mediaMovelCasos',
+	 'mediaMovelMortes'], axis=1)
 
 tabela = tabela.rename(columns=
-                       {'date':'Data',
-                        'last_available_confirmed':'Casos',
-                        'last_available_deaths':'Mortes'})
+					   {'date':'Data',
+						'last_available_confirmed':'Casos',
+						'last_available_deaths':'Mortes'})
 
 # creating charts
 
 tabelaMostra = go.Figure(data=[go.Table(header=dict(values=list(tabela.columns)),
-                                        cells=dict(values=[tabela.Data,
-                                                           tabela.Casos,
-                                                           tabela.Mortes]))])
+										cells=dict(values=[tabela.Data,
+														   tabela.Casos,
+														   tabela.Mortes]))])
 
 mediaCasosDia = go.Figure()
 
 mediaCasosDia.add_trace(
-    go.Scatter(
-        x=caso_full["date"],
-        y=caso_full["mediaMovelCasos"],
-        name="Média Móvel"))
+	go.Scatter(
+		x=caso_full["date"],
+		y=caso_full["mediaMovelCasos"],
+		name="Média Móvel"))
 
 mediaCasosDia.add_trace(
-    go.Bar(
-        x=caso_full["date"],
-        y=caso_full["new_confirmed"],
-        name="Casos no dia"))
+	go.Bar(
+		x=caso_full["date"],
+		y=caso_full["new_confirmed"],
+		name="Casos no dia"))
 
 mediaMortesDia = go.Figure()
 
 mediaMortesDia.add_trace(
-    go.Scatter(
-        x=caso_full["date"],
-        y=caso_full["mediaMovelMortes"],
-        name="Média Móvel"))
+	go.Scatter(
+		x=caso_full["date"],
+		y=caso_full["mediaMovelMortes"],
+		name="Média Móvel"))
 
 mediaMortesDia.add_trace(
-    go.Bar(
-        x=caso_full["date"],
-        y=caso_full["new_deaths"],
-        name="Mortes no dia"))
+	go.Bar(
+		x=caso_full["date"],
+		y=caso_full["new_deaths"],
+		name="Mortes no dia"))
 
 # creating dash app
 
@@ -93,15 +93,18 @@ server = app.server
 app.title = 'Covid-19 Ribeirão Preto'
 
 app.layout = html.Div([
-    html.Div('Atualizações sobre a pandemia de Covid-19 em Ribeirão Preto',
-              style={'textAlign':'center'}),
-    html.Div([html.A(href='mailto:jornalistagustavoribeiro@gmail.com',
-                    children="jornalistagustavoribeiro@gmail.com")],
-              style={'textAlign':'center'}),
-    html.Div([html.A(href='https://github.com/jornalistagustavoribeiro/PainelCovid19RibeiraoPreto',
-                    children="Github")],
-              style={'textAlign':'center'}),
-    html.Div([dcc.Graph(id="casos", figure=mediaCasosDia)]),
-    html.Div([dcc.Graph(id="mortes", figure=mediaMortesDia)]),
-    html.Div([dcc.Graph(id='ultimas', figure=tabelaMostra)])
+	html.Div('Atualizações sobre a pandemia de Covid-19 em Ribeirão Preto',
+			  style={'textAlign':'center'}),
+	html.Div([dcc.Graph(id="casos", figure=mediaCasosDia)]),
+	html.Div([dcc.Graph(id="mortes", figure=mediaMortesDia)]),
+	html.Div([dcc.Graph(id='ultimas', figure=tabelaMostra)]),
+	html.Div([html.A(href='https://github.com/jornalistagustavoribeiro/PainelCovid19RibeiraoPreto',
+					children="Github")],
+			  style={'textAlign':'center'}),
+	html.Div([html.A(href='mailto:jornalistagustavoribeiro@gmail.com',
+					children="jornalistagustavoribeiro@gmail.com")],
+			  style={'textAlign':'center'}),
+	html.Div('Fonte: Secretarias de Saúde das Unidades Federativas, dados tratados por Álvaro Justen e equipe de voluntários Brasil.IO',
+			  style={'textAlign':'center'})
+
 ])
